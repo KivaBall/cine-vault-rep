407e51abd0dd6562ce429e7edd44a2de9288c800
Ihor Kobzar <supermensy@gmail.com>
2025-01-03 16:15:24 +0200

    Move all usings into GlobalUsings.cs

---
+++ src/CineVault.API/GlobalUsings.cs
@@ -0,0 +1,6 @@
+global using CineVault.API.Controllers.Requests;
+global using CineVault.API.Controllers.Responses;
+global using CineVault.API.Entities;
+global using CineVault.API.Extensions;
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.EntityFrameworkCore;
