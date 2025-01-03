55559abab14fde3dafc9809c4ca649099f0a6972
Ihor Kobzar <supermensy@gmail.com>
2025-01-03 16:16:29 +0200

    Add new controller with Environment and Exception methods

---
+++ src/CineVault.API/Controllers/AppInfoController.cs
@@ -0,0 +1,24 @@
+namespace CineVault.API.Controllers;
+
+[Route("api/[action]")]
+public class AppInfoController(IConfiguration configuration) : ControllerBase
+{
+    [HttpGet]
+    public ActionResult<string> Environment()
+    {
+        var environment = configuration["ASPNETCORE_ENVIRONMENT"];
+
+        if (environment == null)
+        {
+            throw new Exception("Something went wrong?");
+        }
+            
+        return Ok($"Current environment => {environment}");
+    }
+
+    [HttpGet]
+    public ActionResult<string> Exception()
+    {
+        throw new Exception("Hello world!");
+    }
+}
