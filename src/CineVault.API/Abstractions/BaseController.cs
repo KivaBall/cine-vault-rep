5e662b9a38a13089a02512f6bc24b9a71df07146
Ihor Kobzar <supermensy@gmail.com>
2025-01-12 16:05:42 +0200

    Add base controller to follow DRY

---
+++ src/CineVault.API/Abstractions/BaseController.cs
@@ -0,0 +1,9 @@
+namespace CineVault.API.Abstractions;
+
+[ApiController]
+[InheritedApiVersion(1, Deprecated = true)]
+[InheritedApiVersion(2)]
+[Route("api/v{v:apiVersion}/[controller]")]
+public abstract class BaseController : ControllerBase
+{
+}
