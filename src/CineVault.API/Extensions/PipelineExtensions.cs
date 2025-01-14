e0c8cc81f415bef5d99bbf340d2d7f38752b7e3c
Ihor Kobzar <supermensy@gmail.com>
2025-01-14 20:51:04 +0200

    Add PipelineExtensions.cs

---
+++ src/CineVault.API/Extensions/PipelineExtensions.cs
@@ -0,0 +1,32 @@
+namespace CineVault.API.Extensions;
+
+public static class PipelineExtensions
+{
+    public static void BuildPipeline(this WebApplication app)
+    {
+        app.UseSwaggerMiddleware();
+
+        if (app.Environment.IsLocal())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        Console.WriteLine($"Current environment is {app.Environment.EnvironmentName}");
+    }
+
+    private static void UseSwaggerMiddleware(this WebApplication app)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+
+            app.UseSwaggerUI();
+        }
+    }
+}
