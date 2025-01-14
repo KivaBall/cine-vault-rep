51c2f76eb371b5890fa3c5a6ba2beb84daf52944
Ihor Kobzar <supermensy@gmail.com>
2025-01-14 20:51:12 +0200

    Add ServicesExtensions

---
+++ src/CineVault.API/Extensions/ServicesExtensions.cs
@@ -0,0 +1,47 @@
+﻿namespace CineVault.API.Extensions;
+
+public static class ServicesExtensions
+{
+    public static void ConfigureServices(this IServiceCollection services,
+        IConfiguration configuration, IHostEnvironment environment)
+    {
+        services.AddDbContext(configuration);
+
+        services.AddEndpoints();
+
+        services.AddSwagger(environment);
+    }
+
+    private static void AddDbContext(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<CineVaultDbContext>(options =>
+        {
+            var connectionString = configuration.GetConnectionString("CineVaultDb");
+
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                throw new InvalidOperationException("Connection string is not configured");
+            }
+
+            options.UseInMemoryDatabase(connectionString);
+        });
+    }
+
+    private static void AddEndpoints(this IServiceCollection services)
+    {
+        services.AddRouting(opt => opt.LowercaseUrls = true);
+
+        services.AddControllers();
+
+        services.AddEndpointsApiExplorer();
+    }
+
+    private static void AddSwagger(this IServiceCollection services,
+        IHostEnvironment environment)
+    {
+        if (environment.IsDevelopment())
+        {
+            services.AddSwaggerGen();
+        }
+    }
+}
