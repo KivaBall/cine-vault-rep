671946a1143dde72a40f1a825982d64dd1351090
Ihor Kobzar <supermensy@gmail.com>
2025-01-22 23:32:48 +0200

    Add new map properties in config for review response

--- src/CineVault.API/Mappers/ReviewProfile.cs
+++ src/CineVault.API/Mappers/ReviewProfile.cs
@@ -11,6 +11,10 @@ public sealed class ReviewProfile : IRegister
             .Map(r => r.MovieTitle,
                 r => r.Movie!.Title)
             .Map(r => r.Username,
-                r => r.User!.Username);
+                r => r.User!.Username)
+            .Map(r => r.Likes,
+                r => r.Reactions.Count(r2 => r2.IsLike))
+            .Map(r => r.Dislikes,
+                r => r.Reactions.Count(r2 => !r2.IsLike));
     }
 }
